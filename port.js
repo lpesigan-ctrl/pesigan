f2a79bf084696ac1159e5b773f93fbfd64c34542
kimchiii <lpesigan@paterostechnologicalcollege.edu.ph>
2026-05-27 21:40:41 +0800

    Update typing effect text in port.js

--- port.js
+++ port.js
@@ -1,4 +1,4 @@
-const text = "We are a group of students who are passionate about technology and programming. We have been working together on various projects and have developed a strong bond as a team. We are always eager to learn new things and take on new challenges. Our goal is to create innovative solutions that can make a positive impact on the world.";
+const text = "I am a student who is passionate about technology and programming. I have been working on various projects and have developed a strong interest in this field. I am always eager to learn new things and take on new challenges. My goal is to create innovative solutions that can make a positive impact on the world.";
 
 let index = 0;
 
